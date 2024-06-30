cf80227315f5b0545b70dca7d7ae2b2f9e239a66
Yusuf Ali <70917481+yusiali@users.noreply.github.com>
2024-06-30 15:35:04 -0500

    Add files via upload
    initial version with ~88% precision after 500 epochs (not optimized yet)

---
+++ mnist_v1.py
@@ -0,0 +1,106 @@
+# MNIST Neural Network without optimization (approx. 88% accuracy after 500 epochs)
+from sklearn.datasets import fetch_openml
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
+
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True, as_frame=False)
+X = X / 255  # normalizes the value
+
+digits = 10
+examples = y.shape[0]
+
+y = y.reshape(1, examples)
+
+Y_new = np.eye(digits)[y.astype('int32')]
+Y_new = Y_new.T.reshape(digits, examples)
+
+m = 60000
+m_test = X.shape[0] - m
+
+X_train, X_test = X[:m].T, X[m:].T
+Y_train, Y_test = Y_new[:, :m], Y_new[:, m:]
+
+shuffle_index = np.random.permutation(m)
+X_train, Y_train = X_train[:, shuffle_index], Y_train[:, shuffle_index]
+
+i = 12
+plt.imshow(X_train[:, i].reshape(28, 28), cmap=matplotlib.cm.binary)
+plt.axis('off')
+plt.show()
+# print(Y_train[:, i])
+c = 0
+for n in Y_train[:, i]:
+    if n == 1:
+        print("Digit:", c)
+    c += 1
+
+
+def compute_multiclass_loss(Y, Y_hat):
+    L_sum = np.sum(np.multiply(Y, np.log(Y_hat)))
+    m = Y.shape[1]
+    L = -(1 / m) * L_sum
+    return L
+
+
+def sigmoid(z):
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+def compute_loss(Y, Y_hat):
+    m = Y.shape[1]
+    L = -(1. / m) * (np.sum(np.multiply(np.log(Y_hat), Y)) + np.sum(np.multiply(np.log(1 - Y_hat), (1 - Y))))
+    return L
+
+
+n_x = X_train.shape[0]
+n_h = 64
+learning_rate = 1
+
+W1 = np.random.randn(n_h, n_x)
+b1 = np.zeros((n_h, 1))
+W2 = np.random.randn(digits, n_h)
+b2 = np.zeros((digits, 1))
+
+X = X_train
+Y = Y_train
+
+for i in range(500):
+    Z1 = np.matmul(W1, X) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.matmul(W2, A1) + b2
+    A2 = np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
+
+    cost = compute_multiclass_loss(Y, A2)
+
+    dZ2 = A2 - Y
+    dW2 = (1. / m) * np.matmul(dZ2, A1.T)
+    db2 = (1. / m) * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.matmul(W2.T, dZ2)
+    dZ1 = dA1 * sigmoid(Z1) * (1 - sigmoid(Z1))
+    dW1 = (1. / m) * np.matmul(dZ1, X.T)
+    db1 = (1. / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+
+    if i % 100 == 0:
+        print("Epoch", i, "cost: ", cost)
+
+print("Final cost: ", cost)
+
+Z1 = np.matmul(W1, X_test) + b1
+A1 = sigmoid(Z1)
+Z2 = np.matmul(W2, A1) + b2
+A2 = np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
+
+predictions = np.argmax(A2, axis=0)
+labels = np.argmax(Y_test, axis=0)
+
+print(confusion_matrix(predictions, labels))
+print(classification_report(predictions, labels))
